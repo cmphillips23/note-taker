2c11e6bb95e7c60ad72fe5069069ece03aa89e2b
Cnidarian <cmphillips23@gmail.com>
2021-03-14 22:35:38 -0500

    created server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
-
+const { db } = require('./db/db');
 
 app.listen(3001, () => {
     console.log('API server now on port 3001!');
