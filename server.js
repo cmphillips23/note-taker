34d97a0e4e61505cee3b9763313f3a3f12c7782c
Cnidarian <cmphillips23@gmail.com>
2021-03-14 23:41:35 -0500

    added routes

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const { db } = require('./db/db');
+const { notes } = require('./db/db');
+const apiRoutes = require('./routes/apiRoutes');
+// const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
+app.use(express.static('public'));
 
 app.listen(3001, () => {
     console.log('API server now on port 3001!');
