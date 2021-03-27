69a6de80c6206e4efd64df3eccc2184e4317da98
Cnidarian <cmphillips23@gmail.com>
2021-03-26 21:48:56 -0500

    added fs readfile to get route

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -2,15 +2,28 @@ const fs = require('fs');
 const shortid = require('shortid');
 
 module.exports = (app) => {
-    app.get('/api', (req, res) => res.send("api routes are cool"));
+    app.get('/api', (req, res) => {
+        
+        fs.readFile('./db/db.json', (err, data) => {
+            if (err) throw err;
+            let note = JSON.parse(data);
+            res.json(note);
+        });
+    });
 
     app.post('/api/notes', (req, res) => {
         let note = { title: req.body.title, text: req.body.text, id: shortid.generate() }
-        let newData = JSON.stringify(note);
 
-        fs.writeFile('./db/db.json', newData, err => {
+        fs.readFile('./db/db.json', (err, data) => {
             if (err) throw err;
-            res.json(newData);
+            let noteString = JSON.parse(data);
+            noteString.push(note);
+            let newData = JSON.stringify(noteString);
+            
+            fs.writeFile('./db/db.json', newData, err => {
+                if (err) throw err;
+                res.json(newData);
+            });
         });
     });
 };
