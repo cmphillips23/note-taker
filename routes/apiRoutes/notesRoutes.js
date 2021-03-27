0ade936588a44efb28a6c710e55a73a74263f89f
Cnidarian <cmphillips23@gmail.com>
2021-03-26 20:28:29 -0500

    added writefile to post route

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -2,38 +2,15 @@ const fs = require('fs');
 const shortid = require('shortid');
 
 module.exports = (app) => {
-    app.get('/api', (req, res) => {
-        res.send("api routes are cool");
-    });
+    app.get('/api', (req, res) => res.send("api routes are cool"));
 
     app.post('/api/notes', (req, res) => {
         let note = { title: req.body.title, text: req.body.text, id: shortid.generate() }
-        console.log(note);
+        let newData = JSON.stringify(note);
+
+        fs.writeFile('./db/db.json', newData, err => {
+            if (err) throw err;
+            res.json(newData);
+        });
     });
 };
-
-/* function createNewNote(body, notes) {
-    const note = body;
-    notes.push(note);
-    fs.writeFileSync(
-      path.join(__dirname, './db/db.json'),
-      JSON.stringify({ notes: notes }, null, 2)
-    );
-    return notes;
-  };
-  
-  app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', (err, data) => {
-      if (err) throw err;
-  
-      let notes = JSON.parse(data);    
-      res.json(notes);
-    });
-  });
-  
-  app.post('/api/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-  
-    const note = createNewNote(req.body, notes);
-    res.json(note);
-  }); */
