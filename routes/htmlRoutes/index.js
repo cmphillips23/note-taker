e36887358afb06dbf471609ee75a82210932af2c
Cnidarian <cmphillips23@gmail.com>
2021-03-26 19:46:45 -0500

    organized html routes

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,12 +1,13 @@
 const path = require('path');
-const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+module.exports = (app) => {
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
-module.exports = router;
+};
--- server.js
+++ server.js
@@ -11,14 +11,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('public'));
 
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
-});
+require('./routes/htmlRoutes')(app);
+// require('./routes/apiRoutes')(app);
 
 function createNewNote(body, notes) {
   const note = body;
