7753a7ac95a1e77730820f0e6ce61d1402c0081e
Cnidarian <cmphillips23@gmail.com>
2021-03-26 19:55:37 -0500

    organized api routes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,29 +1,35 @@
 const fs = require('fs');
 const path = require('path');
 const { notes } = require('../../db/db.json');
-const router = require('express').Router();
 
+module.exports = (app) => {
+    app.get('/api', (req, res) => {
+        res.send("api routes are cool");
+    });
+};
 
-function createNewNote(body, notes) {
+/* function createNewNote(body, notes) {
     const note = body;
     notes.push(note);
     fs.writeFileSync(
-      path.join(__dirname, '../../db/db.json'),
+      path.join(__dirname, './db/db.json'),
       JSON.stringify({ notes: notes }, null, 2)
     );
     return notes;
-};
-
-
-router.get('/notes', (req, res) => {
-    res.json(notes);
-});
-
-router.post('/notes', (req, res) => {
+  };
+  
+  app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+  
+      let notes = JSON.parse(data);    
+      res.json(notes);
+    });
+  });
+  
+  app.post('/api/notes', (req, res) => {
     req.body.id = notes.length.toString();
-
+  
     const note = createNewNote(req.body, notes);
     res.json(note);
-});
-
-module.exports = router;
+  }); */
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const PORT = process.env.port || 3001;
 const app = express();
-const path = require('path');
-const fs = require('fs');
-const { notes } = require('./db/db.json');
 
 
 // Express middleware
@@ -12,33 +9,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 require('./routes/htmlRoutes')(app);
-// require('./routes/apiRoutes')(app);
-
-function createNewNote(body, notes) {
-  const note = body;
-  notes.push(note);
-  fs.writeFileSync(
-    path.join(__dirname, './db/db.json'),
-    JSON.stringify({ notes: notes }, null, 2)
-  );
-  return notes;
-};
-
-app.get('/api/notes', (req, res) => {
-  fs.readFile('./db/db.json', (err, data) => {
-    if (err) throw err;
-
-    let notes = JSON.parse(data);    
-    res.json(notes);
-  });
-});
-
-app.post('/api/notes', (req, res) => {
-  req.body.id = notes.length.toString();
-
-  const note = createNewNote(req.body, notes);
-  res.json(note);
-});
+require('./routes/apiRoutes/notesRoutes')(app);
 
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
